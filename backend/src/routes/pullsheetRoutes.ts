e6732ca7255e474e11197b31de0bfcb1c6cc26ae
Justin Glotzbach <20047549+justinglotz@users.noreply.github.com>
2026-02-09 07:38:55 -0600

    chore: deleted backend subfolder inside of repo

--- backend/src/routes/pullsheetRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { importPullsheet } from '../controllers/pullsheetController.js';
-const router = Router();
-
-router.post('/pullsheet/import', importPullsheet);
-
-export default router;
--- backend/src/routes/rackDrawingRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getRackDrawings } from '../controllers/rackDrawingController.js';
-
-const router = Router();
-
-router.get('/rack-drawings', getRackDrawings);
-
-export default router;
